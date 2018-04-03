2cd613c0e651a4f214ae1a300bc3e3059de6b7c9
fuzhentao <1012560716@qq.com>
2018-04-03 16:45:11 +0800

    配置mysql 数据库

--- myboke/settings.py
+++ myboke/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'myboke.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'HOST': '39.106.14.164',
+        'USER': 'fuzhentao',
+        'PASSWORD': 'fuzhentao',
+        'PORT': '3306',
     }
 }
 
